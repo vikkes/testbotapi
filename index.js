8d04b4196cf5d30599a33200b643d3825709b774
vikkes <vikkes@gmail.com>
2017-02-19 18:52:53 +0100

    5

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText+a ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
+        speech: speech+"a",
         displayText: speech,
         source: 'webhook-echo-sample1'
     });
